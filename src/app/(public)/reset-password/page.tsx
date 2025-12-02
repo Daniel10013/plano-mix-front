36e0ae6ddefa577772f58668a54f663bd3885566
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-12-02 04:19:13 -0300

    Correcao inputs

--- src/app/(public)/reset-password/page.tsx
+++ src/app/(public)/reset-password/page.tsx
@@ -105,11 +105,11 @@ export default function ForgotPassword() {
                     <div className='flex flex-col  items-center justify-center gap-5 w-full xl:w-[75%]'>
                         <div className="flex items-center gap-3 bg-white rounded-[10px] border border-gray-300 w-full focus-within:border-[#8173FF] transition-all duration-200 px-2 py-2">
                             <LockClosedIcon className="h-8 w-8 text-[#a9b1bf]" />
-                            <input type="text" onChange={(e) => { setNewPassword(e.target.value) }} value={newPassword} placeholder="Digite a nova senha" className="outline-none text-[20px] text-gray-700 placeholder-[#a9b1bf]" />
+                            <input type="password" onChange={(e) => { setNewPassword(e.target.value) }} value={newPassword} placeholder="Digite a nova senha" className="outline-none text-[20px] text-gray-700 placeholder-[#a9b1bf]" />
                         </div>
                         <div className="flex items-center gap-3 bg-white rounded-[10px] border border-gray-300 w-full focus-within:border-[#8173FF] transition-all duration-200 px-2 py-2">
                             <LockClosedIcon className="h-8 w-8 text-[#a9b1bf]" />
-                            <input type="text" onChange={(e) => { setConfirmPassword(e.target.value) }} value={confirmPassword} placeholder="Confirme a nova senha" className="outline-none text-[20px] text-gray-700 placeholder-[#a9b1bf]" />
+                            <input type="password" onChange={(e) => { setConfirmPassword(e.target.value) }} value={confirmPassword} placeholder="Confirme a nova senha" className="outline-none text-[20px] text-gray-700 placeholder-[#a9b1bf]" />
                         </div>
                         <button onClick={handleSubmit} disabled={isLoading}
                             className={`py-2 px-1 text-white bg-[#8173FF] text-2xl rounded-[10px] w-full 
