fbefaad3c851aa93d8efd87be830ede98a94c335
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-12-14 01:58:28 -0300

    adicionado listagem e filtro de lojas

--- src/services/visits.service.ts
+++ src/services/visits.service.ts
@@ -1,11 +1,29 @@
 import api from "../lib/api";
-import { Visit } from "../types/Visits/Visits";
+import { Visit, VisitDetails } from "../types/Visits/Visits";
 
 export async function getVisitsByShopping(shoppingId: number) {
     try {
         const response = await api.get(`/visit/shopping/${shoppingId}`);
         const responseData = response.data as {status: boolean, data: Visit[]};
         return responseData;
+    }
+    catch (err: any) {
+        const status = err.response?.status ?? 500;
+        const message = err.response?.data?.message ?? 'Erro ao pegar visitas do shopping!';
+        const customError = new Error(message) as Error & { status?: number };
+        customError.status = status;
+        throw customError;
+    }
+}
+
+export async function getVisitDetails(visitId: number) {
+    try {
+        const response = await api.get(`/visit/details/${visitId}`);
+        const responseData = response.data as {success: boolean, data: VisitDetails};
+        return {
+            status: responseData.success,
+            data: responseData.data 
+        };
     }
     catch (err: any) {
         const status = err.response?.status ?? 500;
