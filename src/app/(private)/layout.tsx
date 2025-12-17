828d2349cbec72b96d0ef27b8b1165d28577c695
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-12-17 19:58:50 -0300

    correcao

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -70,7 +70,7 @@ export default function PrivateLayout({
 
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/get-me`,
+          `${process.env.NEXT_PUBLIC_API_URL}/users/get-me`,
           {
             credentials: 'include',
             cache: 'no-store',
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -46,7 +46,7 @@ export default function PublicLayout({
 
       try {
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/get-me`,
+          `${process.env.NEXT_PUBLIC_API_URL}/users/get-me`,
           {
             credentials: 'include',
             cache: 'no-store',
