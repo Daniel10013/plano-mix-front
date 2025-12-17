49b13b1e5b68ffe5f649cd404d6bc67b98fc8b9b
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-12-17 18:06:33 -0300

    Correcao view mix e adicionado vercel.json

--- src/types/Classifications/Classification.ts
+++ src/types/Classifications/Classification.ts
@@ -2,3 +2,11 @@
 export type Classification = { id: number, name: string }
 export type Segment = { id: number, name: string, classification_id: number }
 export type Activity = { id: number, name: string, segment_id: number }
+export type Mix = {
+    classification_id: number;
+    classification: string;
+    segment_id: number | null;
+    segment: string | null; 
+    activity_id: number | null;
+    activity: string | null;
+};
