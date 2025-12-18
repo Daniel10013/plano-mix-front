9ed57b8d0721cacd77ba39ad7c9c26b2ad09aa66
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-12-18 19:46:15 -0300

    tentantiva de corrigir erro

--- src/app/(private)/shoppings/[id]/page.tsx
+++ src/app/(private)/shoppings/[id]/page.tsx
@@ -1,12 +1,16 @@
 import { redirect } from "next/navigation";
 import Details from "@/src/components/Shoppings/DetailsPage";
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const { id } = params; const numberId = Number(id); 
-    
-    if (!Number.isInteger(numberId) || numberId <= 0) { 
-        redirect("/shoppings"); 
-    } 
-    
-    return <Details shoppingId={numberId} />;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const numberId = Number(id)
+    if (!Number.isInteger(numberId) || numberId <= 0) {
+        redirect("/shoppings");
+    }
+
+    return (
+        <>
+            <Details shoppingId={numberId} />
+        </>
+    )
 }
